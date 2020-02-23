944f33ba94d09b73137fd6891bac734f555d6e39
Dominic Bruton <d.bruton@live.co.uk>
2020-02-23 16:00:40 +0000

    Fix app test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the app title', () => {
+  const wrapper = render(<App />);
+  expect(wrapper.getByText('Loan Calculator')).toBeTruthy();
+});
+
+test('renders the loan input fields', async () => {
+  const wrapper = render(<App />);
+  expect(wrapper.getByText('Amount Requested')).toBeTruthy();
+  expect(wrapper.getByText('£')).toBeTruthy();
+  expect(wrapper.getByText('Duration')).toBeTruthy();
+  expect(wrapper.getByText('months')).toBeTruthy();
+});
+
+test('renders rcf', async () => {
+  const wrapper = render(<App />);
+  expect(wrapper.getByText('Revolving Credit Facility')).toBeTruthy();
+});
+
+test('renders bl', async () => {
+  const wrapper = render(<App />);
+  expect(wrapper.getByText('Business Loan')).toBeTruthy();
 });
