ed9d31078b0ca70df99debba1771929ccced4545
Dominic Bruton <d.bruton@live.co.uk>
2020-02-23 11:11:03 +0000

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    // color: theme.palette.text.secondary
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "center",
+  },
+  grid: {
+    paddingTop: 20
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Loan Calculator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={2} className={classes.grid}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>Input</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>RCF Table</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>BCR Table</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
